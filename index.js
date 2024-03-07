3496933ecf5dd70644ab271d4d0a6576daca25e2
Eugeniu_Fortuna <roomjjket@gmail.com>
2024-03-07 13:43:47 +0200

    Practice 1, practice with object

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('JOOOOOOOOOOOOPA');
+const numbersOfFilms = +prompt('How many films did you watched5');
+
+const personalMovieDB = {
+	count: numbersOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const a = prompt('One of the films which you watched?'),
+	b = prompt('Give it film rating pls'),
+	c = prompt('One of the films which you watched?'),
+	d = prompt('Give it film rating pls');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
