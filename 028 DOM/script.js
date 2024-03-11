85d62402317580c618b779f37e42f1564b49391a
Eugeniu_Fortuna <roomjjket@gmail.com>
2024-03-11 13:39:41 +0200

    028 end, starting 029

--- 028 DOM/script.js
+++ 028 DOM/script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const box = document.querySelector('#box');
+
+const btns = document.querySelectorAll('button');
+
+const btns2 = document.getElementsByTagName('button');
+
+const circles = document.querySelectorAll('.circle');
+
+const hearts = document.querySelectorAll('.heart');
+
+// ========================
+
+console.log(box);
+
+console.log(btns);
+
+console.log(btns[2]);
+
+console.log(btns2);
+
+console.log(btns2[2]);
+
+console.log(circles[2]);
+
+console.log(circles);
+
+console.log(hearts);
