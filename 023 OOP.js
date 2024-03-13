79e05959c837157cc04f12a2f07237ea5667290f
Eugeniu_Fortuna <roomjjket@gmail.com>
2024-03-13 17:34:53 +0200

    OOP practice

---
+++ 023 OOP.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// 023 practice oop
+
+// console.dir([1, 2, 3]);
+
+// const soldier = {
+// 	health: 400,
+// 	armor: 100,
+// 	sayHello: () => {
+// 		console.log('Hello');
+// 	},
+// };
+
+// const john = {
+// 	health: 100,
+// };
+
+// Object.setPrototypeOf(john, soldier); // Assign to obj john, obj soldier (properties).
+// const sirgii = Object.create(soldier); // Create an object with soldier properties.
+
+// soldier.sayHello(); // Hello
+// console.log(john.armor); // 100
+// console.log(sirgii.health); // 400
+
+// sirgii.age = 27;
+
+// console.log(sirgii.age); // 27
