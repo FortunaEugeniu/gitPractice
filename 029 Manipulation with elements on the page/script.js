565dc9d603839ca715772dc90b46d47505a9831d
Eugeniu_Fortuna <roomjjket@gmail.com>
2024-03-13 18:36:55 +0200

    DOM repeat

--- 029 Manipulation with elements on the page/script.js
+++ 029 Manipulation with elements on the page/script.js
@@ -1,34 +1,19 @@
 'use strict';
 
 const box = document.querySelector('#box'),
-	btns = document.querySelectorAll('button'),
+	btn = document.querySelectorAll('button'),
 	circles = document.querySelectorAll('.circle'),
-	hearts = document.querySelectorAll('.heart'),
-	oneHerat = document.querySelector('.heart'),
-	wrapper = document.querySelector('.wrapper');
+	hearts = document.querySelectorAll('.heart');
 
-box.style.backgroundColor = 'purple';
-box.style.width = '250px';
-box.style.height = '250px';
+console.log(box);
 
-btns.forEach(el => (el.style.borderRadius = '100px'));
+console.log(btn);
+console.log(btn[1]);
 
-circles[0].style.backgroundColor = 'red';
-circles[1].style.backgroundColor = 'yellow';
-circles[2].style.backgroundColor = 'green';
+console.log(circles);
+console.log(circles[1]);
 
-const div = document.createElement('div');
-const text = document.createTextNode('I was here'); // rarely used
+console.log(hearts);
+console.log(hearts[1]);
 
-div.classList.add('black');
-
-wrapper.append(div);
-// wrapper.before(div);
-// circles[0].remove();
-
-// hearts[0].replaceWith(circles[2]);
-
-// div.innerHTML = 'JOOPA';
-// div.style.fontSize = '3rem';
-
-div.textContent = 'Hello';
+hearts.forEach(el => console.log(el));
